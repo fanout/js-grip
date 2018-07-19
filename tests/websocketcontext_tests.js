325e2664d0f9b044a3fd28390b23c04b346fc8e9
Justin Karneges <justin@karneges.com>
2018-07-18 21:11:22 -0700

    move websocketcontext from faas-grip to here

---
+++ tests/websocketcontext_tests.js
@@ -0,0 +1,76 @@
+var assert = require('assert');
+var jspack = require('jspack').jspack;
+var grip = require('../lib/grip');
+
+(function testOpen() {
+	var ws = new grip.WebSocketContext('conn-1', {}, [new grip.WebSocketEvent('OPEN')]);
+	assert.equal(ws.id, 'conn-1');
+	assert(ws.isOpening());
+	assert(!ws.canRecv());
+	assert(!ws.accepted);
+	ws.accept();
+	assert(ws.accepted);
+})();
+
+(function testRecv() {
+	var ws = new grip.WebSocketContext('conn-1', {}, [new grip.WebSocketEvent('TEXT', Buffer.from('hello'))]);
+	assert(!ws.isOpening());
+	assert(ws.canRecv());
+	var msg = ws.recv();
+	assert.equal(msg, 'hello');
+	assert(!ws.canRecv());
+})();
+
+(function testSend() {
+	var ws = new grip.WebSocketContext('conn-1', {}, []);
+	assert(!ws.isOpening());
+	assert(!ws.canRecv());
+	assert.equal(ws.outEvents.length, 0);
+	ws.send(Buffer.from('apple'));
+	ws.send('banana');
+	ws.sendBinary(Buffer.from('cherry'));
+	ws.sendBinary('date');
+	assert.equal(ws.outEvents.length, 4);
+	assert.equal(ws.outEvents[0].getType(), 'TEXT');
+	assert(ws.outEvents[0].getContent().equals(Buffer.from('m:apple')));
+	assert.equal(ws.outEvents[1].getType(), 'TEXT');
+	assert(ws.outEvents[1].getContent().equals(Buffer.from('m:banana')));
+	assert.equal(ws.outEvents[2].getType(), 'BINARY');
+	assert(ws.outEvents[2].getContent().equals(Buffer.from('m:cherry')));
+	assert.equal(ws.outEvents[3].getType(), 'BINARY');
+	assert(ws.outEvents[3].getContent().equals(Buffer.from('m:date')));
+})();
+
+(function testControl() {
+	var ws = new grip.WebSocketContext('conn-1', {}, []);
+	assert.equal(ws.outEvents.length, 0);
+	ws.subscribe('foo');
+	ws.unsubscribe('bar');
+	assert.equal(ws.outEvents.length, 2);
+
+	assert.equal(ws.outEvents[0].getType(), 'TEXT');
+	assert(ws.outEvents[0].getContent().toString().startsWith('c:'));
+	assert.deepEqual(JSON.parse(ws.outEvents[0].getContent().slice(2).toString()),
+			{type: 'subscribe', channel: 'foo'});
+
+	assert.equal(ws.outEvents[1].getType(), 'TEXT');
+	assert(ws.outEvents[1].getContent().toString().startsWith('c:'));
+	assert.deepEqual(JSON.parse(ws.outEvents[1].getContent().slice(2).toString()),
+			{type: 'unsubscribe', channel: 'bar'});
+})();
+
+(function testClose() {
+	var ws = new grip.WebSocketContext('conn-1', {}, [new grip.WebSocketEvent('CLOSE', jspack.Pack('>H', [100]))]);
+	assert(!ws.isOpening());
+	assert(ws.canRecv());
+	var msg = ws.recv();
+	assert(msg == null);
+	assert.equal(ws.closeCode, 100);
+
+	var ws = new grip.WebSocketContext('conn-1', {}, []);
+	assert(!ws.isOpening());
+	assert(!ws.canRecv());
+	assert(!ws.closed);
+	ws.close(code=100);
+	assert.equal(ws.outCloseCode, 100);
+})();
