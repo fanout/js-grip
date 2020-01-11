206b333584521b4b7eafd2ce4fedbb769fef48f8
Katsuyuki Ohmuro <harmony7@pex2.jp>
2020-01-11 20:34:35 +0900

    Added ESM build

---
+++ rollup.esm.config.js
@@ -0,0 +1,29 @@
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import json from '@rollup/plugin-json';
+import builtins from 'builtin-modules';
+
+export default {
+    input: 'src/main.mjs',
+    output: {
+        file: 'esm/main.mjs',
+        format: 'esm'
+    },
+    plugins: [
+        commonjs(),
+        json(),
+        babel({
+            babelrc: false,
+            exclude: 'node_modules/**',　// only transpile our source code
+            plugins: [
+                '@babel/plugin-proposal-class-properties',
+            ],
+        }),
+    ],
+    external: [
+        ...builtins,
+        'isomorphic-fetch',
+        'agentkeepalive',
+        'jwt-simple',
+    ],
+};
