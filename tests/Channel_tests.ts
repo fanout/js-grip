041c3a820bd6223736d77636f3260808710cb891
Katsuyuki Ohmuro <harmony7@pex2.jp>
2020-03-23 05:01:46 +0900

    Updated tests

---
+++ tests/Channel_tests.ts
@@ -0,0 +1,37 @@
+import assert from 'assert';
+import 'mocha';
+
+import Channel from '../src/data/Channel';
+
+describe('Channel', function () {
+
+    describe('Initialize', function () {
+        it('should allow creating a Channel with a name', function () {
+            const ch = new Channel('name');
+            assert.equal(ch.name, 'name');
+            assert.equal(ch.prevId, null);
+        });
+        it('should allow creating a Channel with both a name and a prevId', function () {
+            const ch = new Channel('name', 'prev-id');
+            assert.equal(ch.name, 'name');
+            assert.equal(ch.prevId, 'prev-id');
+        })
+    });
+
+    describe('Export', function () {
+        it('should allow exporting a Channel with a name', function () {
+            const ch = new Channel('name');
+            assert.equal(JSON.stringify(ch.export()), JSON.stringify({
+                name: 'name'
+            }));
+        });
+        it('should allow exporting a Chanel with a name and a prevId', function () {
+            const ch = new Channel('name', 'prev-id');
+            assert.equal(JSON.stringify(ch.export()), JSON.stringify({
+                name: 'name', prevId: 'prev-id'
+            }));
+        });
+    });
+
+});
+
