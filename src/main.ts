8f3e59fa132d2b2795cf5130dea85e9bf9ac17e2
Katsuyuki Ohmuro <harmony7@pex2.jp>
2020-06-20 13:45:14 +0900

    Fix main exports and and rollup config

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,25 @@
-import Channel from './data/Channel';
-import Response from './data/Response';
-import Publisher from './engine/Publisher';
-import HttpStreamFormat from './data/http/HttpStreamFormat';
-import HttpResponseFormat from './data/http/HttpResponseFormat';
-import WebSocketContext  from './data/websocket/WebSocketContext';
-import WebSocketEvent from './data/websocket/WebSocketEvent';
-import WebSocketMessageFormat from './data/websocket/WebSocketMessageFormat';
+// Flatten and export
 export * from './gripUtilities';
-
-import IGripConfig from './engine/IGripConfig';
-import IExportedChannel from './data/IExportedChannel';
-import IExportedResponse from './data/IExportedResponse';
-import IHoldInstruction from './IHoldInstruction';
-import IWebSocketEvent from './data/websocket/IWebSocketEvent';
-
-export {
-    Channel,
-    Response,
-    HttpResponseFormat,
-    HttpStreamFormat,
-    WebSocketContext,
-    WebSocketEvent,
-    WebSocketMessageFormat,
-    Publisher,
-};
-
-export type {
-    IGripConfig,
-    IExportedChannel,
-    IExportedResponse,
-    IHoldInstruction,
-    IWebSocketEvent,
-};
+export { default as Publisher } from './engine/Publisher';
+export { default as Auth } from './auth/index';
+export { default as Format } from './data/Format';
+export { default as Item } from './data/Item';
+export { default as PublisherClient } from './engine/PublisherClient';
+export { default as Channel } from './data/Channel';
+export { default as Response } from './data/Response';
+export { default as HttpStreamFormat } from './data/http/HttpStreamFormat';
+export { default as HttpResponseFormat } from './data/http/HttpResponseFormat';
+export { default as WebSocketContext } from './data/websocket/WebSocketContext';
+export { default as WebSocketEvent } from './data/websocket/WebSocketEvent';
+export { default as WebSocketMessageFormat } from './data/websocket/WebSocketMessageFormat';
+export type { default as IGripConfig } from './engine/IGripConfig';
+export type { default as IExportedChannel } from './data/IExportedChannel';
+export type { default as IExportedResponse } from './data/IExportedResponse';
+export type { default as IHoldInstruction } from './IHoldInstruction';
+export type { default as IWebSocketEvent } from './data/websocket/IWebSocketEvent';
+export type { default as IFormat } from './data/IFormat';
+export type { default as IFormatExport } from './data/IFormatExport';
+export type { default as IItem } from './data/IItem';
+export type { default as IItemExport } from './data/IItemExport';
+export type { default as IPublisherConfig } from './engine/IPublisherConfig';
+export type { default as IPublishCallback } from "./engine/IPublishCallback";
