ef2a04cafd8c52c9a0f3489d0a03800d01dfd35f
Katsuyuki Omuro <komuro@fastly.com>
2024-02-19 18:08:21 +0900

    Drop toBuffer()

--- src/utilities/buffer.ts
+++
@@ -1,7 +0,0 @@
-import { Buffer } from 'node:buffer';
-
-// Check if input is a buffer. If not, turn it into a string and then
-// encode the bits of its UTF-8 representation into a new Buffer.
-export function toBuffer(input: any): Buffer {
-    return Buffer.isBuffer(input) ? input : Buffer.from(input.toString(), 'utf8');
-}
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -3,5 +3,4 @@ export * from './webSocketEvents.js';
 export * from './ws-over-http.js';
 export * from './jwt.js';
 export * from './string.js';
-export * from './buffer.js';
 export * from './http.js';
