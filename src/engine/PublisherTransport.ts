bf2e6604a38ec74c3e411855bd43722bc875c25c
Katsuyuki Omuro <komuro@fastly.com>
2024-02-20 15:31:05 +0900

    Drop agentkeepalive for now

--- src/engine/PublisherTransport.ts
+++ src/engine/PublisherTransport.ts
@@ -1,11 +1,7 @@
-import HttpAgent, { HttpsAgent } from 'agentkeepalive';
-
 import { FetchResponse, IPublisherTransport, IReqHeaders } from './index.js';
-import { PublishException } from '../data/index.js';
 
 export class PublisherTransport implements IPublisherTransport {
   publishUri: string;
-  _agent?: HttpAgent;
 
   constructor(uri: string) {
     // Initialize this class with a URL representing the publishing endpoint.
@@ -20,30 +16,11 @@ export class PublisherTransport implements IPublisherTransport {
     this.publishUri = String(new URL('./publish/', url));
   }
 
-  getAgent() {
-    if(this._agent != null) {
-      return this._agent;
-    }
-    const publishUri = new URL(this.publishUri);
-    switch (publishUri.protocol) {
-      case 'http:':
-        this._agent = new HttpAgent();
-        break;
-      case 'https:':
-        this._agent = new HttpsAgent();
-        break;
-      default :
-        throw new PublishException('Bad URI', {statusCode: -2});
-    }
-    return this._agent;
-  }
-
   async publish(headers: IReqHeaders, content: string): Promise<FetchResponse> {
     const reqParams = {
       method: 'POST',
       headers,
       body: content,
-      agent: this.getAgent(),
     };
     return await fetch(this.publishUri, reqParams);
   }
