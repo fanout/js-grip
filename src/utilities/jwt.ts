e8ed76e3089e8b254ef99dd4ad7d05af405698d7
Katsuyuki Omuro <komuro@fastly.com>
2024-02-21 07:09:44 +0900

    Allow use of SPKI public keys for validateSig

--- src/utilities/jwt.ts
+++ src/utilities/jwt.ts
@@ -5,15 +5,22 @@ const textEncoder = new TextEncoder();
 // Validate the specified JWT token and key. This method is used to validate
 // the GRIP-SIG header coming from GRIP proxies such as Pushpin or Fanout.io.
 // Note that the token expiration is also verified.
-export async function validateSig(token: string, key: string | Uint8Array, iss?: string) {
+export async function validateSig(token: string, key: string | Uint8Array | jose.KeyLike, iss?: string) {
 
+    let verifyKey: Uint8Array | jose.KeyLike;
     if (typeof key === 'string') {
-        key = textEncoder.encode(key);
+        if (key.indexOf('-----BEGIN PUBLIC KEY-----') === 0) {
+            verifyKey = await jose.importSPKI(key, 'RS256');
+        } else {
+            verifyKey = textEncoder.encode(key);
+        }
+    } else {
+        verifyKey = key;
     }
 
     let claim: jose.JWTVerifyResult;
     try {
-        claim = await jose.jwtVerify(token, key);
+        claim = await jose.jwtVerify(token, verifyKey);
     } catch (e) {
         return false;
     }
--- test/unit/utilities/jwt.test.ts
+++ test/unit/utilities/jwt.test.ts
@@ -1,7 +1,8 @@
-import { describe, it } from 'node:test';
+import { describe, it, before } from 'node:test';
 import assert from 'node:assert';
 
 import * as jose from 'jose';
+import { PRIVATE_KEY_1, PUBLIC_KEY_1, PUBLIC_KEY_FASTLY } from '../sampleKeys.js';
 
 import { validateSig } from '../../../src/index.js';
 
@@ -9,65 +10,146 @@ const textEncoder = new TextEncoder();
 
 describe('utilities/jwt', () => {
     describe('#validateSig', () => {
-        it('check that a signature can be validated', async () => {
+        describe('Using HS256', () => {
+            it('check that a signature can be validated', async () => {
 
-            const signJwt = new jose.SignJWT({ 'claim': 'hello' })
-              .setProtectedHeader({ alg: 'HS256' })
-              .setExpirationTime('1h');
-            const token = await signJwt.sign(textEncoder.encode('key=='));
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'HS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(textEncoder.encode('key=='));
 
-            assert.ok(await validateSig(token, 'key=='));
+                assert.ok(await validateSig(token, 'key=='));
+            });
+            it("check that a signature can't be validated if it's expired", async () => {
+
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'HS256' })
+                  .setExpirationTime('-1h');
+                const token = await signJwt.sign(textEncoder.encode('key=='));
+
+                assert.ok(!await validateSig(token, 'key=='));
+            });
+            it("check that a signature can't be validated if the key doesn't match", async () => {
+
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'HS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(textEncoder.encode('key=='));
+
+                assert.ok(!await validateSig(token, 'key==='));
+            });
+            it("check the ISS of a claim", async () => {
+
+                const signJwt = new jose.SignJWT({
+                    'claim': 'hello',
+                    'iss': 'foo',
+                  })
+                  .setProtectedHeader({ alg: 'HS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(textEncoder.encode('key=='));
+
+                assert.ok(await validateSig(token, 'key==', 'foo'));
+            });
+            it("check claim with missing ISS won't validate", async () => {
+
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'HS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(textEncoder.encode('key=='));
+
+                assert.ok(!await validateSig(token, 'key==', 'foo'));
+            });
+            it("check claim with mismatched ISS won't validate", async () => {
+
+                const signJwt = new jose.SignJWT({
+                    'claim': 'hello',
+                    'iss': 'bar',
+                  })
+                  .setProtectedHeader({ alg: 'HS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(textEncoder.encode('key=='));
+
+                assert.ok(!await validateSig(token, 'key==', 'foo'));
+            });
         });
-        it("check that a signature can't be validated if it's expired", async () => {
+        describe('Using RS256', () => {
+            let privateKey1: jose.KeyLike;
+            before(async () => {
+                privateKey1 = await jose.importPKCS8(PRIVATE_KEY_1, 'RS256');
+            });
+            it('check that a signature can be validated', async () => {
 
-            const signJwt = new jose.SignJWT({ 'claim': 'hello' })
-              .setProtectedHeader({ alg: 'HS256' })
-              .setExpirationTime('-1h');
-            const token = await signJwt.sign(textEncoder.encode('key=='));
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'RS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(privateKey1);
 
-            assert.ok(!await validateSig(token, 'key=='));
-        });
-        it("check that a signature can't be validated if the key doesn't match", async () => {
+                assert.ok(await validateSig(token, PUBLIC_KEY_1));
+            });
+            it('check that a signature can be validated when the key is loaded beforehand', async () => {
 
-            const signJwt = new jose.SignJWT({ 'claim': 'hello' })
-              .setProtectedHeader({ alg: 'HS256' })
-              .setExpirationTime('1h');
-            const token = await signJwt.sign(textEncoder.encode('key=='));
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'RS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(privateKey1);
 
-            assert.ok(!await validateSig(token, 'key==='));
-        });
-        it("check the ISS of a claim", async () => {
+                const publicKey1 = await jose.importSPKI(PUBLIC_KEY_1, 'RS256');
 
-            const signJwt = new jose.SignJWT({
-                'claim': 'hello',
-                'iss': 'foo',
-              })
-              .setProtectedHeader({ alg: 'HS256' })
-              .setExpirationTime('1h');
-            const token = await signJwt.sign(textEncoder.encode('key=='));
+                assert.ok(await validateSig(token, publicKey1));
+            });
+            it("check that a signature can't be validated if it's expired", async () => {
 
-            assert.ok(await validateSig(token, 'key==', 'foo'));
-        });
-        it("check claim with missing ISS won't validate", async () => {
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'RS256' })
+                  .setExpirationTime('-1h');
+                const token = await signJwt.sign(privateKey1);
 
-            const signJwt = new jose.SignJWT({ 'claim': 'hello' })
-              .setProtectedHeader({ alg: 'HS256' })
-              .setExpirationTime('1h');
-            const token = await signJwt.sign(textEncoder.encode('key=='));
+                assert.ok(!await validateSig(token, PUBLIC_KEY_1));
+            });
+            it("check that a signature can't be validated if the key doesn't match", async () => {
 
-            assert.ok(!await validateSig(token, 'key==', 'foo'));
-        });
-        it("check claim with mismatched ISS won't validate", async () => {
+                const signJwt = new jose.SignJWT({ 'claim': 'hello' })
+                  .setProtectedHeader({ alg: 'RS256' })
+                  .setExpirationTime('1h');
+                const token = await signJwt.sign(privateKey1);
 
-            const signJwt = new jose.SignJWT({
-                'claim': 'hello',
-                'iss': 'bar',
-              })
-              .setProtectedHeader({ alg: 'HS256' })
-              .setExpirationTime('1h');
-            const token = await signJwt.sign(textEncoder.encode('key=='));
+                assert.ok(!await validateSig(token, PUBLIC_KEY_FASTLY));
+            });
+            it("check the ISS of a claim", async () => {
 
-            assert.ok(!await validateSig(token, 'key==', 'foo'));
+                const signJwt = new jose.SignJWT({
+                    'claim': 'hello',
+                    'iss': 'foo',
+                })
+                    .setProtectedHeader({ alg: 'RS256' })
+                    .setExpirationTime('1h');
+                const token = await signJwt.sign(privateKey1);
+
+                assert.ok(await validateSig(token, PUBLIC_KEY_1, 'foo'));
+            });
+            it("check claim with missing ISS won't validate", async () => {
+
+                const signJwt = new jose.SignJWT({
+                    'claim': 'hello',
+                })
+                    .setProtectedHeader({ alg: 'RS256' })
+                    .setExpirationTime('1h');
+                const token = await signJwt.sign(privateKey1);
+
+                assert.ok(!await validateSig(token, PUBLIC_KEY_1, 'foo'));
+            });
+            it("check claim with mismatched ISS won't validate", async () => {
+
+                const signJwt = new jose.SignJWT({
+                    'claim': 'hello',
+                    'iss': 'bar',
+                })
+                    .setProtectedHeader({ alg: 'RS256' })
+                    .setExpirationTime('1h');
+                const token = await signJwt.sign(privateKey1);
+
+                assert.ok(!await validateSig(token, PUBLIC_KEY_1, 'foo'));
+            });
         });
     });
 });
