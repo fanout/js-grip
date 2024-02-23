868625fe7af4fd66512db61786d502c94d04f7d2
Katsuyuki Omuro <komuro@fastly.com>
2024-02-24 06:46:21 +0900

    Export Fastly Fanout public key as constant

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const PUBLIC_KEY_FASTLY_FANOUT_PEM: string = `-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAECKo5A1ebyFcnmVV8SE5On+8G81Jy
+BjSvcrx4VLetWCjuDAmppTo3xM/zz763COTCgHfp/6lPdCyYjjqc+GM7sw==
+-----END PUBLIC KEY-----`;
+
+export const PUBLIC_KEY_FASTLY_FANOUT_JWK: JsonWebKey = {
+  "kty": "EC",
+  "crv": "P-256",
+  "x": "CKo5A1ebyFcnmVV8SE5On-8G81JyBjSvcrx4VLetWCg",
+  "y": "7gwJqaU6N8TP88--twjkwoB36f-pT3QsmI46nPhjO7M"
+};
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * as Auth from './auth/index.js';
 export * from './data/index.js';
 export * from './engine/index.js';
 export * from './utilities/index.js';
+export * from './constants.js';
