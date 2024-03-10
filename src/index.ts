b084f04284e3194ab12a8ee0e91fb664cb412825
Katsuyuki Omuro <komuro@fastly.com>
2024-03-11 02:03:40 +0900

    fanout: Move Fanout public keys to Fastly Fanout export

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@ export * as Auth from './auth/index.js';
 export * from './data/index.js';
 export * from './engine/index.js';
 export * from './utilities/index.js';
-export * from './constants.js';
+
+/**
+ * @deprecated - Should import these from '@fanoutio/grip/fastly-fanout'
+ */
+export * from './fastly-fanout/keys.js';
